9b962bf705266e8ba55d62fd866fe623d815c37e
Rija Ménagé <git@rijam.sent.as>
2015-07-31 19:55:57 +0800

    shell script that start all the serivces. Very simple: string replacement for Database details, permissions and start all services. No need to install plugin here as that can be done from WP admin with latest version and compatibility check with current version of wordpress. Permalinks settings can also be set in WP admin later.

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+  # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
+	# configuring wp-config with DB connection details from linked container then generate random password for keys
+  sed -e "s/database_name_here/$DB_ENV_MYSQL_DATABASE/
+  s/localhost/$DB_PORT_3306_TCP_ADDR/
+  s/username_here/$DB_ENV_MYSQL_USER/
+  s/password_here/$DB_ENV_MYSQL_PASSWORD/
+  /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+  /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+
+
+  chown www-data:www-data /usr/share/nginx/www/wp-config.php
+
+fi
+
+# start all the services
+/usr/local/bin/supervisord -n

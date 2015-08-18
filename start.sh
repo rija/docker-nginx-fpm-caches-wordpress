452e4e7cc35b6ee1bf42194d0c73d06ea003a5f2
Rija Ménagé <git@rijam.sent.as>
2015-08-18 16:13:12 +0800

    Always replace DB connection strings in wp-config.php as sometimes when the docker host is restarted the ip address change and the wordpress site shows a database connection error. When the config file is regenerated whenever the container starts, it always pick up the right ip address to connect to the database. However sometimes in dev, we need to set variables in the wp-config.php (like relocate to true or to disable new version check). Because of that before the wp-config is regenerated, the old one id backed up if it exists. It still is manual work to copy the variables into the generated config.

--- start.sh
+++ start.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
-if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
-  # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-	# configuring wp-config with DB connection details from linked container then generate random password for keys
-  sed -e "s/database_name_here/$DB_ENV_MYSQL_DATABASE/
-  s/localhost/$DB_PORT_3306_TCP_ADDR/
-  s/username_here/$DB_ENV_MYSQL_USER/
-  s/password_here/$DB_ENV_MYSQL_PASSWORD/
-  /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-  /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
 
-
-  chown www-data:www-data /usr/share/nginx/www/wp-config.php
-
+# first backup any existing config file in case variables have been manually added to it
+if [ -f /usr/share/nginx/www/wp-config.php ]; then
+  cp /usr/share/nginx/www/wp-config.php /usr/share/nginx/www/wp-config.php.orig
 fi
 
+
+# Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
+# configuring wp-config with DB connection details from linked container then generate random password for keys
+sed -e "s/database_name_here/$DB_ENV_MYSQL_DATABASE/
+s/localhost/$DB_PORT_3306_TCP_ADDR/
+s/username_here/$DB_ENV_MYSQL_USER/
+s/password_here/$DB_ENV_MYSQL_PASSWORD/
+/'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+/'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
+
+chown www-data:www-data /usr/share/nginx/www/wp-config.php
+
+# to make SSL work, make sure server name is replaced with correct value passed as an ENV upon docker run
 sed -i -e "s/server_name\s*localhost/server_name $SERVER_NAME/g" /etc/nginx/sites-available/default
 
 # start all the services

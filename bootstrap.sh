430d332f270a191f702bd15092b0c59f2a75f94d
Rija Ménagé <gitlab@rijam.ml1.net>
2017-05-15 22:59:47 +0800

    Testing wordpress install in container from Travis-ci
    consolidated all wordpress related config in the install_wordpress script
    cater for reboot so that that wordpress is then git pulled instead of cloned
    reorganised travis config so that the wordpress tests are together

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,31 +7,7 @@ DB_USER=${DB_USER:-$DB_MYSQL_USER}
 DB_PASSWORD=${DB_PASSWORD:-$DB_MYSQL_PASSWORD}
 DB_DATABASE=${DB_DATABASE:-$DB_MYSQL_DATABASE}
 
-echo "$(date): Boostraping a new Wordpress instance for $SERVER_NAME"
-
-
-echo "Backing up existing wp-config in case variables have been manually added to it"
-if [ -f /usr/share/nginx/www/wp-config.php ]; then
-  cp /usr/share/nginx/www/wp-config.php /usr/share/nginx/www/wp-config.php.bak
-fi
-
-
-# Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-# configuring wp-config with DB connection details from linked container then generate random password for keys
-echo "Replacing database connection details in wp-config with $DB_DATABASE, $DB_HOSTNAME, $DB_USER, DB_PASSWORD (hidden)"
-sed -e "s/database_name_here/$DB_DATABASE/
-s/localhost/$DB_HOSTNAME/
-s/username_here/$DB_USER/
-s/password_here/$DB_PASSWORD/
-/'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
-/'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
-
+echo "$(date): Boostraping a new web server instance for $SERVER_NAME"
 
 
 echo "Replacing the placeholder in nginx config files for server name and cert domain name for $SERVER_NAME"
